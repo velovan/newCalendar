414b84c8728d7993a6fb459dc2b2dc7ac0e270b4
velovan <veloshrike@gmail.com>
2016-11-09 14:34:27 +0200

    color day

--- CalendarProject/src/calendarProj/CurrentMonth.java
+++ CalendarProject/src/calendarProj/CurrentMonth.java
@@ -9,6 +9,9 @@ public class CurrentMonth {
 
 	public final static int daysinmonths[] = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_RESET = "\u001B[0m";
+
 	public static void displayMonth(int month, int year) {
 
 		// The number of days to leave blank at
@@ -17,9 +20,7 @@ public class CurrentMonth {
 		LocalDate cldr = LocalDate.now();
 		int blankdays = 0;
 		System.out.println("\n       " + monthcalender[month] + ": " + year);
-
 		System.out.println("___________________________\n ");
-
 		System.out.println("Mon Tue Wed Thu Fri Sat Sun ");
 
 		// Compute how much to leave before before the first day of the month.
@@ -29,18 +30,20 @@ public class CurrentMonth {
 		int daysInMonth = daysinmonths[month];
 
 		// Blank out the labels before 1st day of the
-		// month 
-		for (int i = 0; i < blankdays; i++) { 
-			System.out.print("    ");  
-		}  
+		// month
+		for (int i = 0; i < blankdays; i++) {
+			System.out.print("    ");
+		}
 
 		for (int i = 1; i <= daysInMonth; i++) {
-
-			// This "if" statement is simpler than messing with NumberFormat
+			if (i == cldr.getDayOfMonth()) {
+//				System.out.print("[" + i + "] ");
+				 System.out.print(ANSI_RED + i + ANSI_RESET);
+				i++;
+			}
 			if (i <= 9) {
 				System.out.print(" ");
 			}
-
 			System.out.print(i);
 
 			if ((blankdays + i) % 7 == 0) {
