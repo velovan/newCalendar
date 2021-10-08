e05ae5e631e5242a6419c90c39629837a6540ae2
velovan <velovan@users.noreply.github.com>
2021-10-08 23:32:53 +0300

    Update SelectedMonth.java

--- CalendarProject/src/calendarProj/SelectedMonth.java
+++ CalendarProject/src/calendarProj/SelectedMonth.java
@@ -24,8 +24,7 @@ public class SelectedMonth{
 			month += 12;
 			year--;
 		} //
-		h = (1 + (int) (((month + 1) * 25) / 10.0) + year + (int) (year / 4.0) + 6 * (int) (year / 100.0)
-				+ (int) (year / 400.0)) % 7;
+		h = (1 + (int) (((month + 1) * 25) / 10.0) + year + (int) (year / 4.0) + 6 * (int) (year / 100.0) + (int) (year / 400.0)) % 7;
 		String dayName = "";
 		switch (h) {
 		case 0:
