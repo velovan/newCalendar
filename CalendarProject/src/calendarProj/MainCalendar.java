d1233734bb809276e56d3e7ebd77fddf52b1055f
velovan <velovan@users.noreply.github.com>
2022-03-07 17:00:42 +0200

    Update MainCalendar.java

--- CalendarProject/src/calendarProj/MainCalendar.java
+++ CalendarProject/src/calendarProj/MainCalendar.java
@@ -6,11 +6,11 @@ public class MainCalendar {
 
 	public static void main(String[] args) {
 
-		CurrentMonth moncldr = new CurrentMonth();
+	  CurrentMonth moncldr = new CurrentMonth();
 
-		LocalDate todaycldr = LocalDate.now();
-		moncldr.displayMonth(todaycldr.getMonth().ordinal(), todaycldr.getYear());
-		SelectedMonth sel = new SelectedMonth();
-		sel.testDate();
+	  LocalDate todaycldr = LocalDate.now();
+	  moncldr.displayMonth(todaycldr.getMonth().ordinal(), todaycldr.getYear());
+	  SelectedMonth sel = new SelectedMonth();
+	  sel.testDate();
 	}
 }
